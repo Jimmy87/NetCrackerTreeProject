430d19812b44464f097aade0794b1b7bd2f566da
Dmitry Ishkov <dimanrussian@gmail.com>
2012-10-22 21:53:35 +0400

    New comments

--- netcrackertree/Tree.java
+++ netcrackertree/Tree.java
@@ -17,13 +17,67 @@ public interface Tree{
     //boolean isSorted; // for search optimization
     //List<Tree> children    
     //Tree parent
+    
+    
+    /**
+     * Adds new Tree, switches @isSorted to "false"
+     * @param id ID of a Node to which you want to attach new Tree
+     */
     void add(ID id, Tree tree);
+    
+    
+    /**
+     * Append to parent's list of Tree children of self
+     * and deletes all links which lead to itself
+     */
     void split(); 
+    
+    
+    /**
+     * @param id
+     * @return Tree with specified ID
+     * if tree is sorted then apply different search algorithm 
+     */
     Tree find(ID id);
+    
+    
+    /**
+     * @param data
+     * @return Tree with specified Data
+     * if tree is sorted then apply different search algorithm 
+     */
     Tree find(Data data);
+    
+    
+    /**
+     * @param path where to save current tree
+     */
     void saveTo(File path);
+    
+    
+    /**
+     * @param path
+     * @return Tree from the specified path
+     */
     Tree loadFrom(File path);
+    
+    
+    /**
+     * @return clone of self
+     */
     Tree clone();
+    
+    
+    /**
+     * @param id ID of a Tree, that can be found inside this Tree
+     * @return deep copy of a Tree with specified ID
+     */
     Tree copy(ID id);
+    
+    
+    /**
+     * sorts Tree, so that children ID always bigger than one parent has
+     * switches isSorted to "true"
+     */
     void sort();
 }
